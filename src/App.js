07a7720fe23a716d1dd521e7c20221738aea91ff
anhdang2109 <dangtrannamanh2109@gmail.com>
2021-03-18 00:24:36 +0700

    input html tempalte

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./header";
+import ProductList from "./productList";
+import Payment from "./payment";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body>
+      <main>
+        <Header></Header>
+        <ProductList></ProductList>
+        <Payment></Payment>
+      </main>
+      </body>
   );
 }
 
