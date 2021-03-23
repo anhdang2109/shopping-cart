71df22ed56e8cc5fe6449f9a79daecbf9ed41c92
anhdang2109 <dangtrannamanh2109@gmail.com>
2021-03-23 09:28:58 +0700

    shopping cart 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from "react";
-let PRODUCTS = [
+const PRODUCTS = [
     {
         id: 1,
         name: 'IPHONE XS',
@@ -36,68 +36,123 @@ let PRODUCTS = [
 ]
 
 function App() {
+    let productLists;
+    let promoCode = ['SUMMER','FREE','LUCKY'];
     let totalQuantityNumber = totalQuantity();
     let totalPriceNumber = totalPrice();
     let taxNumber = tax(totalPriceNumber)
     let totalBillNumber = totalBill(totalPriceNumber, taxNumber);
-    let [products, setProducts] = useState(PRODUCTS);
-    let [totalQuantityHook, setTotalQuantityHook] = useState(totalQuantityNumber);
+
+
+    const [products, setProducts] = useState(PRODUCTS);
+    const [totalQuantityHook, setTotalQuantityHook] = useState(totalQuantityNumber);
     const [totalPriceHook, setTotalPriceHook] = useState(totalPriceNumber);
     const [taxHook, setTaxHook] = useState(taxNumber);
     const [totalBillHook, setTotalBillHook] = useState(totalBillNumber);
-    const productLists = products.map((product) =>
-        <li key={product.id} className="row">
-            <div className="col left">
-                <div className="thumbnail">
-                    <a href="/">
-                        <img src={product.image} alt=""/>
-                    </a>
-                </div>
-                <div className="detail">
-                    <div className="name"><a href="/">{product.name}</a></div>
-                    <div className="description">
-                        {product.description}
+    const [discount, setDiscount] = useState(0);
+
+    if (products.length !== 0) {
+        productLists = products.map((product) =>
+            <li key={product.id} className="row">
+                <div className="col left">
+                    <div className="thumbnail">
+                        <a href="/">
+                            <img src={product.image} alt=""/>
+                        </a>
                     </div>
-                    <div className="price">${product.price}</div>
+                    <div className="detail">
+                        <div className="name"><a href="/">{product.name}</a></div>
+                        <div className="description">
+                            {product.description}
+                        </div>
+                        <div className="price">${product.price}</div>
+                    </div>
                 </div>
-            </div>
 
-            <div className="col right">
-                <div className="quantity">
-                    <input type="number" className="quantity" step="1" defaultValue={product.quantity}/>
-                </div>
+                <div className="col right">
+                    <div className="quantity">
+                        <input type="number"
+                               className="quantity"
+                               id=""
+                               step="1"
+                               onChange={event => changeQuantity(product.id,event.target.value)}
+                               defaultValue={product.quantity}/>
+                    </div>
 
-                <div className="remove">
-                    <svg onClick={() => removeProduct(product.id)}
-                         version="1.1"
-                         className="close"
-                         x="0px"
-                         y="0px"
-                         viewBox="0 0 60 60"
-                         enableBackground="new 0 0 60 60"
-                    >
-                        <polygon
-                            points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812"
-                        />
-                    </svg>
+                    <div className="remove">
+                        <svg onClick={() => removeProduct(product.id)}
+                             version="1.1"
+                             className="close"
+                             x="0px"
+                             y="0px"
+                             viewBox="0 0 60 60"
+                             enableBackground="new 0 0 60 60"
+                        >
+                            <polygon
+                                points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812"
+                            />
+                        </svg>
+                    </div>
                 </div>
-            </div>
-        </li>
-    )
+            </li>
+        );
+    }
+    else {
+        productLists = <div><h1>"Không có sản phẩm nào trong giỏ"</h1><button type="button">Quay lại mua hàng</button></div>;
+    }
+
+    function updateData() {
+        setTotalQuantityHook(totalQuantity());
+        let newTotalPrice = totalPrice()
+        let newTax = tax(newTotalPrice)
+        setTotalPriceHook(newTotalPrice);
+        setTaxHook(newTax);
+        setTotalBillHook(totalBill(newTotalPrice, newTax));
+    }
+    function checkPromoCode() {
+        let inputCode = '';
+        let truePromo = false;
+        inputCode = document.getElementById("promo-code").value;
+        for (let i = 0; i < promoCode.length; i++) {
+            if (promoCode[i] === inputCode) truePromo = true
+        }
+        if (truePromo === true) {
+            setDiscount(50)
+            let newTotalPrice = totalPrice()/2
+            let newTax = tax(newTotalPrice)
+            setTotalPriceHook(newTotalPrice);
+            setTaxHook(newTax);
+            setTotalBillHook(totalBill(newTotalPrice, newTax));
+        }
+        else {
+            alert("Bạn nhập không đúng mã giảm giá!")
+            setDiscount(0)
+            let newTotalPrice = totalPrice()
+            let newTax = tax(newTotalPrice)
+            setTotalPriceHook(newTotalPrice);
+            setTaxHook(newTax);
+            setTotalBillHook(totalBill(newTotalPrice, newTax));
+        }
+    }
+    function changeQuantity(id,value) {
+        let selectedProduct = findById(id);
+        let index = PRODUCTS.indexOf(selectedProduct);
+        PRODUCTS[index].quantity = Number(value);
+        updateData();
+    }
     function removeProduct(id) {
+        let newProductsLength = products.length;
         if (window.confirm('Bạn có chắc là muốn xoá sản phẩm này?')) {
-            const selectedProduct = findById(id);
+            let selectedProduct = findById(id);
             let index = PRODUCTS.indexOf(selectedProduct);
             if (index > -1) {
                 setProducts(products => products.filter(product => product.id !== id))
+                newProductsLength -= 1;
             }
-            PRODUCTS = PRODUCTS.filter(product => product.id !== id)
-            setTotalQuantityHook(totalQuantity());
-            let newTotalPrice = totalPrice()
-            let newTax = tax(newTotalPrice)
-            setTotalPriceHook(totalPrice());
-            setTaxHook(tax(newTotalPrice));
-            setTotalBillHook(totalBill(newTotalPrice, newTax));
+            updateData();
+            if (newProductsLength === 0) {
+                document.querySelector("#totalNumber").classList.add("hidden");
+            }
         }
     }
     function findById(id) {
@@ -144,16 +199,17 @@ function App() {
                     {productLists}
                 </ul>
             </section>
-            <section className="container">
+            <section id="totalNumber" className="container">
                 <div className="promotion">
                     <label htmlFor="promo-code">Have A Promo Code?</label>
                     <input type="text" id="promo-code"/>
-                    <button type="button"> </button>
+                    <button type="button" onClick={() => checkPromoCode()}> </button>
                 </div>
 
                 <div className="summary">
                     <ul>
                         <li>Subtotal <span>${totalPriceHook.toFixed(2)}</span></li>
+                        <li>Discount <span>{discount} %</span></li>
                         <li>Tax <span>${taxHook.toFixed(2)}</span></li>
                         <li className="total">Total <span>${totalBillHook.toFixed(2)}</span></li>
                     </ul>
