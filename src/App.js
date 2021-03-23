da052dcd1aa6b805b3602ebdbab3a7125301478a
anhdang2109 <dangtrannamanh2109@gmail.com>
2021-03-23 09:36:09 +0700

    update reset

--- src/App.js
+++ src/App.js
@@ -51,56 +51,11 @@ function App() {
     const [totalBillHook, setTotalBillHook] = useState(totalBillNumber);
     const [discount, setDiscount] = useState(0);
 
-    if (products.length !== 0) {
-        productLists = products.map((product) =>
-            <li key={product.id} className="row">
-                <div className="col left">
-                    <div className="thumbnail">
-                        <a href="/">
-                            <img src={product.image} alt=""/>
-                        </a>
-                    </div>
-                    <div className="detail">
-                        <div className="name"><a href="/">{product.name}</a></div>
-                        <div className="description">
-                            {product.description}
-                        </div>
-                        <div className="price">${product.price}</div>
-                    </div>
-                </div>
 
-                <div className="col right">
-                    <div className="quantity">
-                        <input type="number"
-                               className="quantity"
-                               id=""
-                               step="1"
-                               onChange={event => changeQuantity(product.id,event.target.value)}
-                               defaultValue={product.quantity}/>
-                    </div>
-
-                    <div className="remove">
-                        <svg onClick={() => removeProduct(product.id)}
-                             version="1.1"
-                             className="close"
-                             x="0px"
-                             y="0px"
-                             viewBox="0 0 60 60"
-                             enableBackground="new 0 0 60 60"
-                        >
-                            <polygon
-                                points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812"
-                            />
-                        </svg>
-                    </div>
-                </div>
-            </li>
-        );
+    function reset() {
+        setProducts(PRODUCTS);
+        document.querySelector("#totalNumber").classList.remove("hidden");
     }
-    else {
-        productLists = <div><h1>"Không có sản phẩm nào trong giỏ"</h1><button type="button">Quay lại mua hàng</button></div>;
-    }
-
     function updateData() {
         setTotalQuantityHook(totalQuantity());
         let newTotalPrice = totalPrice()
@@ -181,6 +136,56 @@ function App() {
     }
     function totalBill(totalPrice, tax) {
         return (Number(totalPrice) + Number(tax))
+    }
+
+    if (products.length !== 0) {
+        productLists = products.map((product) =>
+            <li key={product.id} className="row">
+                <div className="col left">
+                    <div className="thumbnail">
+                        <a href="/">
+                            <img src={product.image} alt=""/>
+                        </a>
+                    </div>
+                    <div className="detail">
+                        <div className="name"><a href="/">{product.name}</a></div>
+                        <div className="description">
+                            {product.description}
+                        </div>
+                        <div className="price">${product.price}</div>
+                    </div>
+                </div>
+
+                <div className="col right">
+                    <div className="quantity">
+                        <input type="number"
+                               className="quantity"
+                               id=""
+                               step="1"
+                               onChange={event => changeQuantity(product.id,event.target.value)}
+                               defaultValue={product.quantity}/>
+                    </div>
+
+                    <div className="remove">
+                        <svg onClick={() => removeProduct(product.id)}
+                             version="1.1"
+                             className="close"
+                             x="0px"
+                             y="0px"
+                             viewBox="0 0 60 60"
+                             enableBackground="new 0 0 60 60"
+                        >
+                            <polygon
+                                points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            </li>
+        );
+    }
+    else {
+        productLists = <div><h1>"Không có sản phẩm nào trong giỏ"</h1><button onClick={() => reset()} type="button">Quay lại mua hàng</button></div>;
     }
     return (
         <main>
